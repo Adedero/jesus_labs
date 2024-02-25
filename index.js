eddfaa191813bc6332840478f4f244874adb3328
Adedero <118962395+Adedero@users.noreply.github.com>
2024-02-25 13:10:32 +0100

    Added audio

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const menu = document.querySelector('#menu-btn')
-const close = document.querySelector('#close-btn')
-const nav = document.querySelector('nav')
-const links = document.querySelectorAll('.navigation a')
-
-links.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        links.forEach((link) => link.classList.remove('active'))
-        link.classList.add('active')   
-    })
-})
-
-menu.addEventListener('click', () => {
-    nav.classList.add('opened')
-})
-
-close.addEventListener('click', removeNav)
-nav.addEventListener('click', removeNav)
-
-function removeNav() {
-    nav.classList.remove('opened')
+const audio = document.querySelector("audio");
+audio.volume = 0.7;
+function stopMusic() {
+    audio.paused? audio.play(): audio.pause()
 }
+
+window.addEventListener("DOMContentLoaded", event => {
+    
+    audio.play();
+    
+    const menu = document.querySelector('#menu-btn')
+    const close = document.querySelector('#close-btn')
+    const nav = document.querySelector('nav')
+    const links = document.querySelectorAll('.navigation a')
+
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            links.forEach((link) => link.classList.remove('active'))
+            link.classList.add('active')   
+        })
+    })
+
+    menu.addEventListener('click', () => {
+        nav.classList.add('opened')
+    })
+
+    close.addEventListener('click', removeNav)
+    nav.addEventListener('click', removeNav)
+
+    function removeNav() {
+        nav.classList.remove('opened')
+    }
+
+});
+
+
+
